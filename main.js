9c437a149222caffd700b40c8e3e08694d26f09f
Meheady <hmmehedi55@gmail.com>
2021-09-20 22:06:38 +0100

    update js

--- main.js
+++ main.js
@@ -11,16 +11,28 @@ profileForm.addEventListener('submit',function(e){
     const ageVal = inAge.value;
     const profVal = inProf.value;
 
-    const content = ` <div class="profileSection">
-                    <p>Name: ${nameVal}</p>
-                    <p>Age: ${ageVal}</p>
-                    <p>Profession: ${profVal}</p>
-                </div>`
-    profile.innerHTML += content;
 
+    if(nameVal==''){
+        alert("Vai nam ta likhen age");
+    }
+    else if(ageVal==''){
+        alert("Vai age ta likhen plz");
+    }
+    else if(profVal==''){
+        alert("Apnar pesa likhen vai");
+    }
+    else{
+        const content = ` <div class="profileSection">
+        <p>Name: ${nameVal}</p>
+        <p>Age: ${ageVal}</p>
+        <p>Profession: ${profVal}</p>
+    </div>`
+    
+    profile.innerHTML += content;
+    
     inName.value = '';
     inAge.value = '';
     inProf.value = '';
-
-
+    
+    }
 })
